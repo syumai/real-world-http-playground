c3700cd520f1ecf9e176e7ebe7bc7a846f2c5e22
syumai <syumai@gmail.com>
2018-10-03 17:58:48 +0900

    Added cookie example

---
+++ cookie/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+const (
+	accessCountKey = "access_count"
+)
+
+const htmlTpl = `<!doctype html>
+<html>
+	<body>
+		<div>You've been to this page for {{.Count}} times.</div>
+		<div>
+			<button onclick="window.location='/';">
+				Reload
+			</button>
+			<form method="POST" action="/reset">
+				<button>Reset</button>
+			</form>
+		</div>
+	</body>
+</html>`
+
+var tmpl *template.Template
+
+func init() {
+	var err error
+	tmpl, err = template.New("html").Parse(htmlTpl)
+	if err != nil {
+		log.Fatalf("%#v", err)
+	}
+}
+
+func setCountCookie(w http.ResponseWriter, cnt int) {
+	c := strconv.Itoa(cnt)
+	http.SetCookie(w, &http.Cookie{
+		Name:  accessCountKey,
+		Value: c,
+	})
+}
+
+func render(w http.ResponseWriter, cnt int) error {
+	return tmpl.Execute(w, struct{ Count int }{Count: cnt})
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	var cnt int
+
+	if ck, err := r.Cookie(accessCountKey); err == nil {
+		cnt, _ = strconv.Atoi(ck.Value)
+	}
+
+	cnt++
+	setCountCookie(w, cnt)
+
+	err := render(w, cnt)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Internal server error"))
+	}
+}
+
+func resetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+	}
+
+	setCountCookie(w, 0)
+
+	err := render(w, 0)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Internal server error"))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", countHandler)
+	http.HandleFunc("/reset", resetHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
